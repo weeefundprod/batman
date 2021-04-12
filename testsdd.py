33ff400e6bb05f80fa28a519ab354b2b047bfd4e
weeefundprod <yanis@weeefund.fr>
2021-04-12 17:03:48 +0200

    delete test file

--- testsdd.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-
-s  = os.environ['MODEL_HHD'].splitlines()
-print(s)
-print(type(s))
--- uuid_test.py
+++
@@ -1,3 +0,0 @@
-import shortuuid
-print(shortuuid.uuid())
-print('test'+shortuuid.ShortUUID().random(length=5))
