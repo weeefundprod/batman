f392748912bc357c0ef47d77c0b031884f3dbe71
weeefundprod <yanis@weeefund.fr>
2021-04-13 16:16:10 +0200

    update readme and processor variante

--- push_to_database.py
+++ push_to_database.py
@@ -21,8 +21,8 @@ try:
 
     if alreadyExits == False:
         try:
-            sql = "INSERT INTO product (SERIAL_NUMBER, MODEL, SKU, VENDOR, SCREEN, HHD_SSD, GRAPHIC_CARD, WEBCAM, BLUETOOTH, DVD, INTERNAL_NUMBER) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-            val = (serial_number, product, sku, vendor, screen, ', '.join(array_hhd_sdd), graphic_card, webcam, bluetooth, dvd, internal_number)
+            sql = "INSERT INTO product (SERIAL_NUMBER, MODEL, SKU, VENDOR, SCREEN, PROCESSOR, HHD_SSD, GRAPHIC_CARD, WEBCAM, BLUETOOTH, DVD, INTERNAL_NUMBER) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)"
+            val = (serial_number, product, sku, vendor, screen, processor, ', '.join(array_hhd_sdd), graphic_card, webcam, bluetooth, dvd, internal_number)
             cursor.execute( sql,val)
             print("Entree en base du produit ", product, "serial number", serial_number)
         except sqlite3.Error as er:
